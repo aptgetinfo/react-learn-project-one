992227b74226a7a850402ae9824b08b2500531da
Deepak Kumar <deepak.2002.02.16@gmail.com>
2021-07-15 18:56:27 +0530

    adding button

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import AddUsers from './components/Users/AddUsers'
+
+function App() {
+  return (
+    <div>
+      <AddUsers></AddUsers>
+    </div>
+  );
+}
+
+export default App;
